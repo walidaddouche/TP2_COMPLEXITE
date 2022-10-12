bd803a16d4dbfeb23c8f5db9160a42be164f55e3
ADDOUCHE Walid <73178026+walidaddouche@users.noreply.github.com>
2022-10-12 17:08:20 +0200

    BASE PROJET 1

---
+++ src/SAT/PROJET1/Literal.java
@@ -0,0 +1,45 @@
+package SAT.PROJET1;
+
+import java.util.ArrayList;
+
+public class Literal {
+    private int sommet;
+    private ArrayList<Literal> literalRelation;
+    // cette liste contient tout les litteraux qui sont en relation avec ce litteral
+
+
+    public Literal(int sommet) {
+        this.sommet = sommet;
+        this.literalRelation = new ArrayList<>();
+    }
+
+
+    public ArrayList<Literal> getLiteralRelation() {
+        return literalRelation;
+    }
+
+
+    public int getSommet() {
+        return sommet;
+    }
+
+
+    public void addNeighbour(Literal literal) {
+        // il ajoute un literal dans sa liste de Relation
+        // ce qui veut dire que ce litteral avec this forment une clause
+        for (Literal ignored : this.getLiteralRelation()) {
+            // Verifie si le literal qu'on souhaite ajouter n'est pas  a l'interieur
+            if (ignored.getSommet() == literal.getSommet())
+                return;
+        }
+        this.literalRelation.add(literal);
+    }
+
+
+    @Override
+    public String toString() {
+        if (sommet < 0)
+            return "¬x" + -1 * sommet;
+        return "x" + sommet;
+    }
+}
