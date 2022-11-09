f97168ef72404c0ffe27674265affa1e90f19bfe
Floshox <florianmario181000@gmail.com>
2022-11-09 17:11:38 +0100

    Lecture Sudoku

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import SAT.PROJET3.Sudoku;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        Sudoku sudoku = new Sudoku("src/SAT/PROJET3/sudoku.txt");
     }
 }
--- src/SAT/PROJET3/Sudoku.java
+++ src/SAT/PROJET3/Sudoku.java
@@ -1,4 +1,48 @@
 package SAT.PROJET3;
 
+import SAT.PROJET1.Formula;
+
+import java.io.*;
+
+//
+
+//
+
+/*
+Représenter solution :
+Ex si on a que 2 ou 9 pour une case spécifique, on la représente de cette manière :
+-Vij1 ^ Vij2 ^ -Vij3 ^ -Vij4 ^ -Vij5 ^ -Vij6 ^ -Vij7 ^ -Vij8 ^ -Vij9  V
+-Vij1 ^ -Vij2 ^ -Vij3 ^ -Vij4 ^ -Vij5 ^ -Vij6 ^ -Vij7 ^ -Vij8 ^ Vij9
+
+ */
+
 public class Sudoku {
+    String[][] grille;
+
+    public Sudoku(String fileSudoku) throws Exception {
+        BufferedReader reader = new BufferedReader(new FileReader(fileSudoku));
+        String currentLine;
+        int tailleZone = Integer.parseInt(reader.readLine());
+        int tailleSudoku = (int) Math.pow(tailleZone,2);
+        grille = new String[tailleSudoku][tailleSudoku];
+
+        int index = 0;
+        while((currentLine = reader.readLine()) != null){
+            grille[index] = currentLine.split(" ");
+            if(grille[index].length < tailleSudoku){
+                throw new Exception("Le sudoku n'a pas assez de lignes pour atteindre la taille demandé, qui est de " + tailleSudoku);
+            }
+            else if(grille[index].length > tailleSudoku){
+                throw new Exception("Le sudoku dépasse le nombre de colonnes autorisé");
+            }
+            index++;
+        }
+        if(index < tailleSudoku){
+            throw new Exception("Le sudoku n'a pas assez de lignes pour atteindre la taille demandé, qui est de " + tailleSudoku);
+        }
+        else if(index > tailleSudoku){
+            throw new Exception("Le sudoku dépasse le nombre de lignes autorisé");
+        }
+    }
+
 }
